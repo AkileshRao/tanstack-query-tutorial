f78115bf719bee92e4529c324b188421ad792378
AkileshRao <akileshrao19@gmail.com>
2023-12-04 07:30:25 +0530

    Added useQuery hook

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import WithQuery from './pages/WithQuery.jsx';
 import WithoutQuery from './pages/WithoutQuery.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const router = createBrowserRouter([
   {
@@ -21,8 +22,13 @@ const router = createBrowserRouter([
   }
 ]);
 
+const client = new QueryClient();
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
-  </React.StrictMode>,
+  <QueryClientProvider client={client}>
+    <React.StrictMode>
+      <RouterProvider router={router}></RouterProvider>
+    </React.StrictMode>,
+  </QueryClientProvider>
 )
